d8f24890ada46844a83cf2d9225c5427915b7c18
AsmaaHelmi <96530441+AsmaaHelmi@users.noreply.github.com>
2026-05-24 19:51:30 +0300

    Delete App/views directory

--- App/views/Admin/layouts/footer.php
+++
@@ -1,9 +0,0 @@
-		<footer class="main-footer">
-				<strong>Copyright &copy; 2024 Clinic Management All rights reserved.</strong>
-			</footer>
-		</div>
-		<script src="/clinic_project/App/Views/Admin/assets/plugins/jquery/jquery.min.js"></script>
-		<script src="/clinic_project/App/Views/Admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-		<script src="/clinic_project/App/Views/Admin/assets/js/adminlte.min.js"></script>
-	</body>
-</html>
