d3c89ba3bfdb1bc11101990d70f812b0189eaa05
AsmaaHelmi <96530441+AsmaaHelmi@users.noreply.github.com>
2026-06-01 18:51:43 +0300

    Update footer.php

--- App/Views/Admin/layouts/footer.php
+++ App/Views/Admin/layouts/footer.php
@@ -2,8 +2,8 @@
 				<strong>Copyright &copy; 2024 Clinic Management All rights reserved.</strong>
 			</footer>
 		</div>
-		<script src="/clinic_project/App/Views/Admin/assets/plugins/jquery/jquery.min.js"></script>
-		<script src="/clinic_project/App/Views/Admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-		<script src="/clinic_project/App/Views/Admin/assets/js/adminlte.min.js"></script>
+		<script src="<?= BASE_URL ?>"/App/Views/Admin/assets/plugins/jquery/jquery.min.js"></script>
+		<script src="<?= BASE_URL ?>"/App/Views/Admin/assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<script src="<?= BASE_URL ?>"/App/Views/Admin/assets/js/adminlte.min.js"></script>
 	</body>
 </html>
